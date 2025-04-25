32b2aae16f985315d276f1fbd861dfc27800b00c
kizadl <dzakiyyanyannas@gmail.com>
2025-04-25 20:34:04 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -722,16 +722,10 @@
     </div>
   </div>
 
-  <footer class="d-footer">
-  <div class="row align-items-center justify-content-between">
-    <div class="col-auto">
-      <p class="mb-0">© 2024 WowDash. All Rights Reserved.</p>
-    </div>
-    <div class="col-auto">
-      <p class="mb-0">Made by <span class="text-primary-600">wowtheme7</span></p>
-    </div>
-  </div>
-</footer>
+{{-- Footer Start --}}
+<x-footer></x-footer>
+{{-- Footer End --}}
+
 </main>
   
   <!-- jQuery library js -->
