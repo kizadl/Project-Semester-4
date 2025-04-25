21fe8bb1faa6a439ac60c9beb1d8f6ef9c1b334d
kizadl <dzakiyyanyannas@gmail.com>
2025-04-25 20:00:42 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,47 +39,7 @@
 </head>
   <body>
 
-{{-- Sidebar Start --}}
-<aside class="sidebar">
-  <button type="button" class="sidebar-close-btn">
-    <iconify-icon icon="radix-icons:cross-2"></iconify-icon>
-  </button>
-  <div>
-    <a href="index.html" class="sidebar-logo">
-      <img src="assets/images/logo.png" alt="site logo" class="light-logo">
-      <img src="assets/images/logo-light.png" alt="site logo" class="dark-logo">
-      <img src="assets/images/logo-icon.png" alt="site logo" class="logo-icon">
-    </a>
-  </div>
-  <div class="sidebar-menu-area">
-    <ul class="sidebar-menu" id="sidebar-menu">
-      <li>
-        <a href="/index">
-          <iconify-icon icon="solar:home-smile-angle-outline" class="menu-icon"></iconify-icon>
-          <span>Dashboard</span>
-        </a>
-      </li>
 
-      <li class="sidebar-menu-group-title">Application</li>
-
-      <li>
-        <a href="/klasifikasi">
-          <iconify-icon icon="solar:document-text-outline" class="menu-icon"></iconify-icon>
-          <span>Klasifikasi</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="/riwayat">
-          <iconify-icon icon="mingcute:storage-line" class="menu-icon"></iconify-icon>
-          <span>Riwayat</span>
-        </a>
-      </li>
-      
-    </ul>
-  </div>
-</aside>
-{{-- Sidebar End --}}
 
 {{-- Dashboard-Main Start --}}
 <main class="dashboard-main">
