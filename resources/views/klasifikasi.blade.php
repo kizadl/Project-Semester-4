40eb33b74abc92dbdbae6f03c7d36b1fbca81055
kizadl <dzakiyyanyannas@gmail.com>
2025-04-25 20:48:55 +0700

    Update klasifikasi.blade.php

--- resources/views/klasifikasi.blade.php
+++ resources/views/klasifikasi.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Wowdash - Bootstrap 5 Admin Dashboard HTML Template</title>
+  <title>MyHealthy - Klasidikasi</title>
   <link rel="icon" type="image/png" href="assets/images/favicon.png" sizes="16x16">
   <!-- remix icon font css  -->
   <link rel="stylesheet" href="assets/css/remixicon.css">
@@ -48,16 +48,16 @@
 
   <div class="dashboard-main-body">
     <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
-  <h6 class="fw-semibold mb-0">Blank Page</h6>
+  <h6 class="fw-semibold mb-0">Klasifikasi</h6>
   <ul class="d-flex align-items-center gap-2">
     <li class="fw-medium">
       <a href="index.html" class="d-flex align-items-center gap-1 hover-text-primary">
         <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
-        Dashboard
+        Application
       </a>
     </li>
     <li>-</li>
-    <li class="fw-medium">Blank Page</li>
+    <li class="fw-medium">Klasifikasi</li>
   </ul>
 </div>
   </div>
