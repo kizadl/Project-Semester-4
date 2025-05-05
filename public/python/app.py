6d133dbf6ae8447a7fa357d8d08bc7fa82699b73
Hido <hidoujin@gmail.com>
2025-05-05 12:07:29 +0700

    Membuat API Model

---
+++ public/python/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("C:/Laragon/www/Project-Semester-4/public/python/model_nb.pkl")
+scaler = joblib.load("C:/Laragon/www/Project-Semester-4/public/python/scaler.pkl")
+
+@app.route('/user/klasifikasi', methods=['POST'])
+def predict():
+    data = request.get_json()
+
+    try:
+        features = [
+            data['usia'],
+            data['jenis_kelamin'],
+            data['tipe_nyeri_dada'],
+            data['tekanan_darah_istirahat'],
+            data['kadar_kolesterol'],
+            data['detak_jantung_maksimum'],
+            data['nyeri_dada_olahraga']
+        ]
+
+        sample = np.array([features])
+        sample_scaled = scaler.transform(sample)
+        pred = model.predict(sample_scaled)
+
+        result = "Punya Penyakit Jantung" if pred[0] == 1 else "Tidak Punya Penyakit Jantung"
+
+        return jsonify({"prediction": result})
+
+    except KeyError as e:
+        return jsonify({"error": f"Missing field: {str(e)}"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
