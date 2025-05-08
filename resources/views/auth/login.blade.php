5bdaa39937df605e9e90622660e3a00bfddea3b9
kizadl <dzakiyyanyannas@gmail.com>
2025-05-08 21:23:33 +0700

    pindah mongo ganti model

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,9 +48,6 @@
     <div class="auth-right py-32 px-24 d-flex flex-column justify-content-center">
         <div class="max-w-464-px mx-auto w-100">
             <div>
-                <a href="index.html" class="mb-40 max-w-290-px">
-                    <img src="assets/images/logo.png" alt="">
-                </a>
                 <h4 class="mb-12">Sign In to your Account</h4>
                 <p class="mb-32 text-secondary-light text-lg">Welcome back! please enter your detail</p>
             </div>
@@ -83,16 +80,6 @@
 
                 <div class="mt-32 center-border-horizontal text-center">
                     <span class="bg-base z-1 px-4">Or sign in with</span>
-                </div>
-                <div class="mt-32 d-flex align-items-center gap-3">
-                    <button type="button" class="fw-semibold text-primary-light py-16 px-24 w-50 border radius-12 text-md d-flex align-items-center justify-content-center gap-12 line-height-1 bg-hover-primary-50"> 
-                        <iconify-icon icon="ic:baseline-facebook" class="text-primary-600 text-xl line-height-1"></iconify-icon>
-                        Google
-                    </button>
-                    <button type="button" class="fw-semibold text-primary-light py-16 px-24 w-50 border radius-12 text-md d-flex align-items-center justify-content-center gap-12 line-height-1 bg-hover-primary-50"> 
-                        <iconify-icon icon="logos:google-icon" class="text-primary-600 text-xl line-height-1"></iconify-icon>
-                        Google
-                    </button>
                 </div>
                 <div class="mt-32 text-center text-sm">
                     <p class="mb-0">Don’t have an account? <a href="/register" class="text-primary-600 fw-semibold">Sign Up</a></p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,9 +48,6 @@
     <div class="auth-right py-32 px-24 d-flex flex-column justify-content-center">
         <div class="max-w-464-px mx-auto w-100">
             <div>
-                <a href="index.html" class="mb-40 max-w-290-px">
-                    <img src="assets/images/logo.png" alt="">
-                </a>
                 <h4 class="mb-12">Sign Up to your Account</h4>
                 <p class="mb-32 text-secondary-light text-lg">Welcome back! please enter your detail</p>
             </div>
@@ -106,33 +103,11 @@
                         <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#password-confirm"></span>
                     </div>
                 </div>
-                <div class="">
-                    <div class="d-flex justify-content-between gap-2">
-                        <div class="form-check style-check d-flex align-items-start">
-                            <input class="form-check-input border border-neutral-300 mt-4" type="checkbox" value="" id="condition">
-                            <label class="form-check-label text-sm" for="condition">
-                                By creating an account means you agree to the
-                                <a href="javascript:void(0)" class="text-primary-600 fw-semibold">Terms & Conditions</a> and our
-                                <a href="javascript:void(0)" class="text-primary-600 fw-semibold">Privacy Policy</a>
-                            </label>
-                        </div>
-                    </div>
-                </div>
             
                 <button type="submit" class="btn btn-primary text-sm btn-sm px-12 py-16 w-100 radius-12 mt-32"> Sign Up</button>
             
                 <div class="mt-32 center-border-horizontal text-center">
                     <span class="bg-base z-1 px-4">Or sign up with</span>
-                </div>
-                <div class="mt-32 d-flex align-items-center gap-3">
-                    <button type="button" class="fw-semibold text-primary-light py-16 px-24 w-50 border radius-12 text-md d-flex align-items-center justify-content-center gap-12 line-height-1 bg-hover-primary-50">
-                        <iconify-icon icon="ic:baseline-facebook" class="text-primary-600 text-xl line-height-1"></iconify-icon>
-                        Google
-                    </button>
-                    <button type="button" class="fw-semibold text-primary-light py-16 px-24 w-50 border radius-12 text-md d-flex align-items-center justify-content-center gap-12 line-height-1 bg-hover-primary-50">
-                        <iconify-icon icon="logos:google-icon" class="text-primary-600 text-xl line-height-1"></iconify-icon>
-                        Google
-                    </button>
                 </div>
                 <div class="mt-32 text-center text-sm">
                     <p class="mb-0">Already have an account? <a href="/login" class="text-primary-600 fw-semibold">Sign In</a></p>
